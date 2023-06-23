a8f8b0ccc31d142b132d36f0bdd2bdafe4da9161
Black8Catgit <safaa.chahid2000@gmail.com>
2023-06-22 18:47:39 -0700

    add FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main -  prints the numbers from 1 to 100
@@ -12,15 +12,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 == 0)
+		if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else
+		if (i % 3 != 0 && i % 5 != 0)
 			printf("%d", i);
 
 		if (i != 100)
