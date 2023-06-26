df17f8ac0fc30332e60164203abfdec2a64cc898
Black8Catgit <safaa.chahid2000@gmail.com>
2023-06-26 00:11:33 -0700

    a function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ *
+ * @str: a character
+ *
+ * Return: 0 (Success)
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
