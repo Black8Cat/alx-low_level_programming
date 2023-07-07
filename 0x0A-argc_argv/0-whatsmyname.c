0d2179a9f9d6c813c688d8d52702b7eea70a193d
Black8Catgit <safaa.chahid2000@gmail.com>
2023-07-07 05:30:55 -0700

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ *
+ * @argv: array
+ * @argc: integer
+ *
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
