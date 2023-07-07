fe532f9a0d407ee49738ce00a60e4c3b468f18f5
Black8Catgit <safaa.chahid2000@gmail.com>
2023-07-07 07:50:02 -0700

    a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - a program that adds positive numbers
+ *
+ * @argc: integer
+ * @argv: array
+ *
+ * Return: return 1 if one of the number contains symbols
+ * that are not digits
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j];)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				j++;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
