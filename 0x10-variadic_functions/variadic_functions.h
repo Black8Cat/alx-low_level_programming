e475d983004952d42c2b365ecfcf8e53f8cb6852
Black8Catgit <safaa.chahid2000@gmail.com>
2023-07-21 02:46:39 -0700

    a function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,13 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_H
+#define VARIADIC_H
 
-#include <unistd.h>
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
+
 
 int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
 
 #endif
